6f45db201f9b42e18b8f1f3e5f6b2e79d21c598e
ZachSumners <114012865+ZachSumners@users.noreply.github.com>
2025-09-28 20:11:35 -0400

    Update SimulationFunctions.py
    Vectorizing/improving old code.

--- SimulationFunctions.py
+++ SimulationFunctions.py
@@ -3,6 +3,7 @@ import InitialConditions
 import math
 import numpy as np
 from numba import jit
+import matplotlib.pyplot as plt
 
 #Calculates how much deformation occured between two locations.
 def deformation_calc(structure):
@@ -12,23 +13,21 @@ def deformation_calc(structure):
     return (highest - lowest)
 
 #Simulation initialization. Generate continuous turbulent structure. Eddy shape currently limited to a circle (technically a hyperbola if you count the value of the grid point).
-def spawn_structure(starting_x, starting_y, rows, cols, radius, x, y):
-        #Track which pixels are apart of the eddy
-        structure = []
-        eddy = np.zeros((rows+1,cols+1))
+def spawn_structure(starting_y, starting_x, rows, cols, radius, x, y):
+        #Initialize eddy
+        eddy = np.zeros((rows,cols))
 
+        #Check to see if it's out of bounds
         if starting_x < radius or starting_x > rows-radius or starting_y < radius or starting_y > rows-radius:
             print("Invalid eddy coordinates.")
             return [[0]]
 
-        for x_val in x:
-            for y_val in y:
-                if ((x_val-(starting_x))**2+(y_val-(starting_y))**2 <= radius**2):
-                    eddy[int(x_val)][int(y_val)] = 2*(1-(x_val-starting_x)**2/radius**2 - (y_val-starting_y)**2/radius**2)
-                    #Add eddy pixels to list for tracking.
-                    structure.append([int(x_val), int(y_val)])
-        
-        return (eddy, structure)
+        #Generate eddy with a mask and parametric equation of circle brightness (darker on edges)
+        Y, X = np.ogrid[:rows, :cols]
+        mask = ((X-starting_x)**2+(Y-starting_y)**2) <= radius**2
+        eddy = 2*(1-(X-starting_x)**2/radius**2 - (Y-starting_y)**2/radius**2) * mask
+
+        return eddy, mask
 
 #Function to find the nearest discrete step to the transducer location (for target values greater than input)
 def find_nearest_above(array, target):
@@ -46,7 +45,7 @@ def find_nearest_below(array, target):
 def time_cross(closest, location, rows, starting_x):
     difference = location - closest
     
-    speed = math.floor(VelocityFunction(starting_x, rows))
+    speed = math.floor(VelocityFunction(rows, starting_x))
     crossed = difference/speed
     return crossed
 
@@ -59,7 +58,6 @@ def group_velocity_value(means, location1, location2, rows, starting_x):
 
     loc1cross = abs(time_cross(means[middleloc1], location1, rows, starting_x))
     loc2cross = time_cross(means[middleloc2], location2, rows, starting_x)
-    
 
     return (loc1cross + fullsteps + loc2cross)
 
@@ -74,23 +72,39 @@ def deformation_value(means, deformations, location1, location2):
     return (def2 - def1)
 
 #SIMULATION VELOCITY FUNCTION.
-@jit(nopython=True)
-def VelocityFunction(i, size):
-    return 2*10*(1-((i-size/2)/(size/2))**2)
+#@jit(nopython=True)
+def VelocityFunction(size, x=None):
+    if x == None:
+        x = np.arange(0, size)
+    return 2*10*(1-((x-size/2)/(size/2))**2)
 
 #Probably the most important function. Moves all grid points according to velocity function for each frame. GPU accelerated.
-@jit(nopython=True)
+#@jit(nopython=True)
 def flow(z, rows, noise, multiplier):
-    for i in range(len(z)):
-        shift = multiplier*math.floor(VelocityFunction(i, rows))
-        #shift = multiplier*math.floor(-(i-500/2)**2/5000 + 12.5)
-        if shift == 0 and i != 0 and i != 499:
-            shift = 1
-        for j in range(shift):
-            if noise == True:
-                z[i] = np.append([2*np.random.random()-1], z[i][:-1]) #Between -1 and 1
-                #z[i] = np.append([np.random.random()], z[i][:-1]) #Between 0 and 1
-            else:
-                z[i] = np.append([0], z[i][:-1])
+    #Calculate the shift in each column of the array because flow not uniform
+    shift = multiplier*np.round(VelocityFunction(rows))
+    shift = np.where(shift == 0, 1, shift)
+    shift = np.asarray(shift, dtype=np.int64)
+
+    #Get array shape
+    rows, cols = z.shape
+    cols_arr = np.arange(cols, dtype=np.int64)
+
+    #Move columns by unique shift number by numpy broadcasting
+    shifted_cols = cols_arr[None, :] - shift[:, None] 
+    mask = (shifted_cols < 0) | (shifted_cols >= cols) 
+    shifted_cols_clipped = np.clip(shifted_cols, 0, cols - 1)
+
+    #Fill each column with however many points went out of bounds (so things are moving in one direction at different rates)
+    z_shifted = z[np.arange(rows)[:, None], shifted_cols_clipped]
     
+    #Either fill with noise or 0s.
+    if noise == True:
+        new = 2*np.random.random(z.shape)-1
+        z_shifted[mask] = new[mask]
+    else:
+        z_shifted[mask] = 0
+    
+    z = z_shifted
+
     return z
